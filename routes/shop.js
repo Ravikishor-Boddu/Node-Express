85c213296cdddd2c0d4d46626d6089d9ef29d20e
Ravikishor Boddu <RavikishorB@citiustech.com>
2020-01-10 15:19:33 +0530

    node-express ejs template engines

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,12 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods:products, docTitle:'Shop'});
+  res.render('shop', {
+    pageTitle: 'Shop',
+    path: '/',
+    prods: products,
+    docTitle: 'Shop'
+  });
 });
 
 module.exports = router;
